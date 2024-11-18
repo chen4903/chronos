8a8195a33bb4b9b49e7ffb176eee0a0e1a9206e2
chenqin <490358423@qq.com>
2024-11-18 23:41:21 +0800

    transfer SOL

--- src/contracts/contracts.ts
+++ src/contracts/contracts.ts
@@ -1,14 +1,31 @@
-import { Commitment, Connection, PublicKey} from '@solana/web3.js';
+import { Commitment, Connection, PublicKey, SystemProgram, Transaction, Keypair, sendAndConfirmTransaction } from '@solana/web3.js';
 
 export class Contracts {
     public connection: Connection;
+    public wallet: Keypair
 
-    constructor(rpcUrl: string, commitment: Commitment) {
+    constructor(rpcUrl: string, commitment: Commitment, wallet: Keypair) {
         this.connection = new Connection(rpcUrl, commitment);
+        this.wallet = wallet
     }
 
     public async getSOLBalance(publicKey: PublicKey): Promise<number> {
         // human friendly: ${balance / LAMPORTS_PER_SOL} SOL
         return await this.connection.getBalance(publicKey);
+    }
+
+    public async transferSOL(toAddress: PublicKey, value: number): Promise<string> {
+        const transaction = new Transaction();
+
+        const instruction = SystemProgram.transfer({
+            fromPubkey: this.wallet.publicKey,
+            toPubkey: toAddress,
+            lamports: value,
+        });
+        transaction.add(instruction);
+
+        const signature = await sendAndConfirmTransaction(this.connection, transaction, [this.wallet]);
+
+        return signature
     }
 }
--- tests/contracts.test.ts
+++ tests/contracts.test.ts
@@ -1,19 +1,38 @@
-import { PublicKey, LAMPORTS_PER_SOL } from '@solana/web3.js';
+import { PublicKey, LAMPORTS_PER_SOL, Keypair } from '@solana/web3.js';
 import { Contracts } from '../src/contracts/contracts';
 import { assert } from 'console';
+import fs from "fs";
 
 describe('Contracts Class', () => {
-    it('getSOLBalance', async () => {
+    const loadWallet = () => {
+        const secretKeyPath = "/Users/levi/.config/solana/id.json";
+        const secretKey = Uint8Array.from(JSON.parse(fs.readFileSync(secretKeyPath, "utf-8")));
+        return Keypair.fromSecretKey(secretKey);
+    };
 
-        const contract = new Contracts("https://api.mainnet-beta.solana.com", 'finalized');
+    const initializeContract = (network: 'test' | 'main') => {
+        const wallet = loadWallet();
+        const endpoint = network === 'test' 
+            ? "https://api.devnet.solana.com"  // Test network
+            : "https://api.mainnet-beta.solana.com"; // Main network
+        return new Contracts(endpoint, 'finalized', wallet);
+    };
 
-        // Create a mock public key
-        const publicKey = new PublicKey('5er33fVk9MqEP3xzQnpcgdo5KWCT4jzP7sbuGPL4bPsN');
-
-        // Call getUserInfo
+    it('getSOLBalance on Testnet', async () => {
+        const contract = initializeContract('test');
+        const publicKey = new PublicKey('FMzZ3PRuFPns7DmrU5aCFDeaSgSVmMtofVJGBJcFJCxr');
         const balance = await contract.getSOLBalance(publicKey);
 
         console.log(`My SOL balance: ${balance / LAMPORTS_PER_SOL} SOL`);
         assert(balance >= 0, "Balance should be greater than or equal to 0");
+    });
+
+
+    it('transferSOL on Testnet', async () => {
+        const contract = initializeContract('test');
+        const toAddress = contract.wallet.publicKey; // Transfer to self
+        const signature = await contract.transferSOL(toAddress, 100);
+
+        console.log(`Transaction signature: ${signature}`);
     });
 });
